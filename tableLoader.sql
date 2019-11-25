3d74c01b40b420f53f5a8624173601ceb63feb8d
Edgarvillafuerte <edgarvillafuerte@discover.com>
2019-11-25 14:11:24 -0600

    .

--- tableLoader.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO USERS (phoneNum, firstName , lastName)
-VALUES (911, "Sparky", "Dawg");
-
-INSERT INTO SONG ( title, bandArtist. fileID)
-VALUES ("Never Gonna Give You Up", "Rick Astley", 3)
-
-Rick Astley - Never Gonna Give You Up
