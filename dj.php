b4ea33c1763ab7450ccfd45d739fc3f356e17b34
aleenaahmad85 <51351132+aleenaahmad85@users.noreply.github.com>
2019-12-03 21:39:51 -0600

    Add files via upload

--- dj.php
+++ dj.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 	$password = "1985Aug25";
     $username = "z1858089";
 try { // if something goes wrong, an exception is thrown
-    
+
 	$dsn = "mysql:host=courses;dbname=z1858089";
 	$pdo = new PDO($dsn, $username, $password);
 }
@@ -11,112 +11,184 @@ catch(PDOexception $e) { // handle that exception
 }
 ?>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>DJ View</title>
-	</head>
-	<body class="grey">
-		<div class="top-buffer"></div>
-		<div class="banner">
-			<h1 class="banner-title">DJ View</h1>
-		</div>
-		<form id="DJ1" method="post" action="dj.php">
-			<h1 class="table-header">Paid Queue</h1>
-			<div class="card">
-				<table class="result-table">
-					<tbody>
-<?php
-//Paid add
-$sql = "SELECT DISTINCT SELECTS.amount, SONG.bandArtist, SONG.title, FILE.fileID, USER.firstName, SELECTS.timeDate FROM USER, SONG, FILE, SELECTS WHERE amount !=0 ORDER BY amount DESC;";
-//$sql = "SELECT userID, fileID, timeDate, amount from SELECTS where amount !=0 ORDER BY amount, timeDate; ";
+<head>
+    <meta charset="UTF-8">
+    <title>DJ View</title>
+</head>
+<body class="grey">
+    <div class="top-buffer"></div>
+    <div class="banner">
+        <h1 class="banner-title">DJ View</h1>
+    </div>
+    <form id="DJ1" method="post" action="dj.php">
+        <h1 class="table-header">Paid Queue</h1>
+        <div class="card">
+            <table class="result-table">
+                <tbody>
+                    <?php
+                    //Paid add
+                    //$sql = "SELECT DISTINCT USER.firstName, SELECTS.amount, SONG.bandArtist, SONG.title, FILE.fileID,  SELECTS.timeDate FROM USER, SONG, FILE, SELECTS WHERE amount > 0 ORDER BY USER.firstName DESc ;";
+                      $sql = "SELECT U.firstName, SN.title, SN.bandArtist, F.fileID, F.version, SL.amount, F.version
+                            FROM USER AS U
+                            INNER JOIN SELECTS AS SL
+                            ON U.USERID = SL.USERID
+                            INNER JOIN FILE AS F
+                            ON SL.FILEID = F.FILEID
+                            INNER JOIN SONG SN
+                            ON F.SONGID = SN.SONGID
+                           WHERE SL.AMOUNT > 0;";                    
 
-$result = $pdo->query($sql);
-//while ($re = $result->fetch(pdo::FETCH_BOTH))
-echo "<table border =1>";
-echo '<td width = "70">Select</td>';
-echo '<td width = "70">Title</td>';
-echo '<td width = "70">Artist</td>';
-echo '<td width = "70">Name</td>';
-echo '<td width = "70">FileID</td>';
-echo '<td width = "70">Ammount</td>';
-echo "<table/>";
+                    //$sql = "SELECT userID, fileID, timeDate, amount from SELECTS where amount !=0 ORDER BY amount, timeDate; ";
 
-foreach ($result as $rows){
-	echo "<table border =1>";
-	//button to select
-	echo '<td width = "70">';
-	echo '<input type = "radio" name = "selected"';
-	echo '</td>';
-	echo '<td width = "70">';
-	echo $rows['title'];
-	echo '</td>';
-	echo '<td width = "70">';
-	echo $rows['bandArtist'];
-	echo '</td>';
-	echo '<td width = "70">';
-	echo $rows['firstName'];
-	echo '</td>';
-	echo '<td width = "70">';
-	echo $rows['fileID'];
-	echo '</td>';
-	echo '<td width = "70">';
-	echo $rows['amount'];
-	echo '</td>';
-	echo "<table/>";
-}
+                    $result = $pdo->query($sql);
+                    //while ($re = $result->fetch(pdo::FETCH_BOTH))
+                    echo "
+                    <table border=1>
+                        ";
+                        echo '
+                        <td width="70">Select</td>';
+                        echo '
+                        <td width="70">Title</td>';
+                        echo '
+                        <td width="70">Version</td>';
+                        echo '
+                        <td width="70">Artist</td>';
+                        echo '
+                        <td width="70">Name</td>';
+                        echo '
+                        <td width="70">FileID</td>';
+                        echo '
+                        <td width="70">Amount</td>';
+                        echo "
+                        <table />";
 
-echo "</table>";
-echo "<br/>";
-?>
-					</tbody>
-				</table>
-			</div>
-		</form>
-		<form id="DJ2" method="post" action="dj.php">
-			<h1 class="table-header">Free Queue</h1>
-			<div class="card">
-				<table class="result-table">
-					<tbody>
-<?php
+                        foreach ($result as $rows){
+                        echo "
+                        <table border=1>
+                            ";
+                            //button to select
+                            echo '
+                            <td width="70">
+                                ';
+                                echo '<input type="radio" name="selected" ';
+	echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['title'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['version'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['bandArtist'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['firstName'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['fileID'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td width="70">
+                                ';
+                                echo $rows['amount'];
+                                echo '
+                            </td>';
+                            echo "
+                            <table />";
+                            }
 
-//free
-$sql = "SELECT userID, fileID, timeDate, amount from SELECTS where amount =0 ORDER BY timeDate; ";
+                            echo "
+                        </table>";
+                        echo "
+                        <br />";
+                        ?>
+                </tbody>
+            </table>
+        </div>
+    </form>
+    <form id="DJ2" method="post" action="dj.php">
+        <h1 class="table-header">Free Queue</h1>
+        <div class="card">
+            <table class="result-table">
+                <tbody>
+                    <?php
 
-$result = $pdo->query($sql);
-echo "<table border =1>";
-echo '<td>Select</td>';
-echo '<td>Title</td>';
-echo '<td>Artist</td>';
-echo '<td>Name</td>';
-echo '<td>FileID</td>';
-echo "<table/>";
+                    //free
+                    $sql = "SELECT userID, fileID, timeDate, amount from SELECTS where amount =0 ORDER BY timeDate; ";
 
-foreach ($result as $rows){
-	echo "<table border =1>";
-	//button to select
-	echo '<td>';
-	echo $rows['userID'];
-	echo '</td>';
-	echo '<td>';
-	echo $rows['fileID'];
-	echo '</td>';
-	echo '<td>';
-	echo $rows['timeDate'];
-	echo '</td>';
-	echo "<table/>";
-}
+                    $result = $pdo->query($sql);
+                    echo "
+                    <table border=1>
+                        ";
+                        echo '
+                        <td>Select</td>';
+                        echo '
+                        <td>Title</td>';
+                        echo '
+                        <td>Artist</td>';
+                        echo '
+                        <td>Name</td>';
+                        echo '
+                        <td>FileID</td>';
+                        echo "
+                        <table />";
 
-echo "</table>";
-echo "<br/>";
-?>
-					</tbody>
-				</table>
-			</div>
-		</form>
-		<div class="bottom-buffer"></div>
-		<div class="bottom-bar">
-			<input type="submit" class="result-submit" value="Clear Paid Queue" form="DJ1">
-			<input type="submit" class="result-submit" value="Clear Free Queue" form="DJ2">
-		</div>
-	</body>
+                        foreach ($result as $rows){
+                        echo "
+                        <table border=1>
+                            ";
+                            //button to select
+                            echo '
+                            <td>
+                                ';
+                                echo $rows['userID'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td>
+                                ';
+                                echo $rows['fileID'];
+                                echo '
+                            </td>';
+                            echo '
+                            <td>
+                                ';
+                                echo $rows['timeDate'];
+                                echo '
+                            </td>';
+                            echo "
+                            <table />";
+                            }
+
+                            echo "
+                        </table>";
+                        echo "
+                        <br />";
+                        ?>
+                </tbody>
+            </table>
+        </div>
+    </form>
+    <div class="bottom-buffer"></div>
+    <div class="bottom-bar">
+        <input type="submit" class="result-submit" value="Clear Paid Queue" form="DJ1">
+        <input type="submit" class="result-submit" value="Clear Free Queue" form="DJ2">
+    </div>
+</body>
 </html>
