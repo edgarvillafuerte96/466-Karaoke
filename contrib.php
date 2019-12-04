ee4157203a4f8b227621a07d7b54e46e4990185f
Sucho34 <46727727+Sucho34@users.noreply.github.com>
2019-12-04 12:47:41 -0600

    Update contrib.php

--- contrib.php
+++ contrib.php
@@ -1,1 +1,4 @@
 $sql = "SELECT * FROM SONG inner join TYPECONTRIBUTOR on TYPECONTRIBUTOR.contributorID = '$typecontributor' AND SONG.songID = TYPECONTRIBUTOR.songID inner join FILES on SONG.songID = FILES.songID;";
+
+
+ echo"<tr align='center'><td>".$title."</td><td>".$bandArtist."</td><td>".$fileID."</td><td>".$version."</td><td>"."<input type='radio' name='fileID' value='$fileID'></td></tr>";
